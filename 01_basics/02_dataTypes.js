0023f6c4e84c45d16b945b6c2f068810baed8857
kartikeya531 <159617229+kartikeya531@users.noreply.github.com>
2026-04-24 12:06:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // terat all js code as newer version
+
+//alert("hello")  // we are using nodejs, not browser
+
+let name = "kartikeya"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// bigint 
+// stirng => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined)  // undefined
+
+console.log(typeof null)  //ibject
