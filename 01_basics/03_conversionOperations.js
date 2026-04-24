c233b848bb5af1aa1abeb14c1c0e0d7fd93bff18
kartikeya531 <159617229+kartikeya531@users.noreply.github.com>
2026-04-24 12:46:42 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = 33
+let marks = "33"
+let a = null
+console.log(typeof score)
+console.log(typeof(score))
+
+console.log(typeof marks)
+console.log(typeof(marks))
+
+let valueInnumber = Number(marks)
+console.log(typeof valueInnumber) 
+console.log(valueInnumber)
+
+let val = Number(a)
+console.log(val)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;  false => 0
+
+let isLoggedIn = 1;
+let isLoggedIn2 = "";
+let isLoggedIn3 = "kartikeya"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+let boolIsLoggedIn2 = Boolean(isLoggedIn2)
+let boolIsLoggedIn3 = Boolean(isLoggedIn3)
+
+console.log(boolIsLoggedIn)
+console.log(boolIsLoggedIn2)
+console.log(boolIsLoggedIn3)
+console.log(typeof(boolIsLoggedIn3))
